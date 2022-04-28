138e8e1861f21eb9bc61d1a43bc70db9a95c4596
pacosanchezm <paco.sanchezm@gmail.com>
2022-04-28 10:14:44 -0500

    ajustes

--- src/Appi.jsx
+++ src/Appi.jsx
@@ -7,7 +7,7 @@ import { Container, Col, Row } from 'react-bootstrap'
 /** @jsxRuntime classic */
 /** @jsx jsx */
 
-import { ThemeProvider, jsx, Styled, useThemeUI, MenuButton } from "theme-ui"
+import {  ThemeProvider, jsx, Styled, useThemeUI, MenuButton } from "theme-ui"
 import { Grid, Flex, Box, Button, Text, Image, Spinner, Input } from "@theme-ui/components"
 import { useMediaQuery } from 'react-responsive'
 
@@ -18,6 +18,7 @@ import {Helmet} from "react-helmet"
 import Theme from "./theme"
 
 import "./styles.css"
+import SideBar from "./sidebar";
 
 
 import Vimeo from '@u-wave/react-vimeo'
@@ -317,6 +318,7 @@ const HeaderBody = props => {
 const Body = props => {
 
   const useacciones = new useAcciones(StateContext)
+  const isDesktop = useMediaQuery({ minWidth: 550 })
 
 // ------------
 
@@ -326,22 +328,28 @@ const Body = props => {
     try {
       return (
 
-        <Container>
+        <Container >
+          <Row className="justify-content-md-center">
 
-          <Helmet>
-            <meta charSet="utf-8" />
-            <title>Únete a la Red Empresando</title>
-            {/* <link rel="canonical" href="http://mysite.com/example" /> */}
-            <meta property="og:title" content="Únete a la Red Empresando" />
-            <meta property="og:description" content="¿Te gustaría recibir lo mas destacado del mundo emprendedor?" />
-            <meta property="og:image" content="https://smxblogs.com/empresando/wp-content/empresando/logoemp.png" />
-          </Helmet>
+            <Helmet>
+              <meta charSet="utf-8" />
+              <title>Únete a la Red Empresando</title>
+              {/* <link rel="canonical" href="http://mysite.com/example" /> */}
+              <meta property="og:title" content="Eventos Empresando" />
+              <meta property="og:description" content="Eventos Empresando" />
+              <meta property="og:image" content="https://smxblogs.com/empresando/wp-content/empresando/logoemp.png" />
+            </Helmet>
 
 
-          <Vimeo
-            video="600286885"
-            autoplay
-          />
+            <Vimeo
+              video="600286885"
+              // autoplay
+              style={{textAlign:"center"}}
+              width={isDesktop ? 640:300}
+              showTitle={isDesktop ? true:false}
+
+            />
+          </Row>
 
         </Container>
 
@@ -441,87 +449,50 @@ export default (App = props => {
 
   return (
 
-      <div id="App">
+    <div id="App">
 
-        {isDesktop ? <div/>
-          :  <div>
-              {/* <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} /> */}
-            </div>                 
-        }
+      <ContextProvider>
 
+        <header sx={{width: "100%"}}>
+            <HeaderBody {...props} />
 
-        <div id="page-wrap">
+            {isDesktop ? 
+                <div>
+                  <MenuBody {...props} />
+                </div>                 
+              : <div>
+                  <SideBar pageWrapId={"page-wrap"} outerContainerId={"App"} />
+                </div>     
+            }
 
-          <div style={{display: 'flex'}}
-          >
-              <ContextProvider>
+        </header>
 
-                <Flex bg="white"
-                  sx={{
-                    flexDirection: "column",
-                    width: "100%",
-                    //minHeight: '300vh',
-                    //justifyContent: 'center'
-                  }}
-                 // css={{ maxWidth: "768px", minWidth: "410px" }}
+        <Container >
 
-                >
-                  <header sx={{width: "100%"}}>
-                    <HeaderBody {...props} />
+            <Row className="justify-content-md-center">
+                <Body {...props} />
+            </Row>
 
-                    {isDesktop ? 
-                        <div>
-                          <MenuBody {...props} />
-                        </div>                 
-                      : <div/>
-                    }
-
-                  </header>
-
-                  <main>
-                    <Body {...props} />
-                  </main>
+        </Container>
 
 
-                  <footer sx={{width: "100%"}}>
-                    <div
-                      sx={{
-                        display: "block",
-                        padding: "10px",
-                        paddingLeft: "10px",
-                        height: "40px",
-                        width: "100%"
-                      }}
-                    />
 
-                    <div
-                      style={{
-                        backgroundColor: "gray",
-                        fontSize: "20px",
-                        color: "white",
-                        borderTop: "1px solid #E7E7E7",
-                        textAlign: "left",
-                        paddingLeft: "20px",
-                        position: "fixed",
-                        left: "0",
-                        bottom: "0",
-                        height: "50px",
-                        width: "100%"
-                      }}
-                    >
-                      <Pie {...props} />
-                    </div>
-                  </footer>
+        <footer sx={{width: "100%"}}>
+
+          <Container >
+
+            <Pie {...props} />
+
+          </Container>
+
+        </footer>
 
 
-                </Flex>
 
-              </ContextProvider>
-          </div>
 
-        </div>
-      </div>
+      </ContextProvider>
 
+    </div>
 
   );
 });
